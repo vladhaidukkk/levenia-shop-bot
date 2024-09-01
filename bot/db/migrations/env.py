b91d4e4a3af8d0f8ca55afde9bacad0de44d54a1
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-09-01 11:50:38 +0300

    create base class for orm models

--- bot/db/migrations/env.py
+++ bot/db/migrations/env.py
@@ -7,13 +7,14 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from bot.config import settings
+from bot.db.models import ModelBase
 
 config = context.config
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-target_metadata = None
+target_metadata = ModelBase.metadata
 
 config.set_main_option("sqlalchemy.url", settings.db.url)
 
