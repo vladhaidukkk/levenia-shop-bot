9ba75fabf16ad17d8039d693bd31b503ef51d99d
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-09-04 08:13:21 +0300

    create command to cancel any survey

--- bot/handlers/surveys/__init__.py
+++ bot/handlers/surveys/__init__.py
@@ -1,6 +1,20 @@
 from aiogram import Router
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
+
+from bot.db.models import UserModel
+from bot.keyboards.root import build_root_keyboard
 
 from .change_role import router as change_role_survey_router
 
 router = Router(name=__name__)
+
+
+@router.message(Command("cancel"))
+async def cancel_command_handler(message: Message, state: FSMContext, user: UserModel) -> None:
+    await state.clear()
+    await message.answer(text="🚫 Поточна дія скасована.", reply_markup=build_root_keyboard(role=user.role))
+
+
 router.include_routers(change_role_survey_router)
